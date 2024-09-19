723bc1ef61ceebe897761d2bf9a069653318a1fc
Jfossy7 <jfossy7@gmail.com>
2024-09-18 22:05:06 -0400

    Test

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 </style>
 <nav class="navbar navbar-expand-xl navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="./index.php">Material Harbor </a>
+        <a class="navbar-brand fw-bold" href="./index.php">Material Harbor</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
